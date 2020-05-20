98e36a6f951ac64519d12aa722050d1d3f4826fe
Wilmer Lin <unitygameacademy@gmail.com>
2020-05-20 15:07:02 -0700

    first version of PickupOnTriggerSystem with Debug.Log

---
+++ Assets/Scripts/Systems/PickupOnTriggerSystem.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+public class PickupOnTriggerSystem : JobComponentSystem
+{
+    private BuildPhysicsWorld buildPhysicsWorld;
+    private StepPhysicsWorld stepPhysicsWorld;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    }
+
+
+    struct PickupOnTriggerSystemJob : ITriggerEventsJob
+    {
+        [ReadOnly] public ComponentDataFromEntity<PickupTag> allPickups;
+        [ReadOnly] public ComponentDataFromEntity<PlayerTag> allPlayers;
+
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            Entity entityA = triggerEvent.Entities.EntityA;
+            Entity entityB = triggerEvent.Entities.EntityB;
+
+            if (allPickups.Exists(entityA) && allPlayers.Exists(entityB))
+            {
+                UnityEngine.Debug.Log("entity A " + entityA + " has collided with entityB " + entityB);
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        PickupOnTriggerSystemJob pickupJob = new PickupOnTriggerSystemJob();
+        pickupJob.allPlayers = GetComponentDataFromEntity<PlayerTag>();
+        pickupJob.allPickups = GetComponentDataFromEntity<PickupTag>();
+
+        JobHandle jobHandle = pickupJob.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld,
+            inputDependencies);
+
+        jobHandle.Complete();
+
+        return jobHandle;
+    }
+}
