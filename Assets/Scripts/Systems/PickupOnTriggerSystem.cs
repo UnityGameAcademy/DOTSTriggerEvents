9dc9b3f28af7ac3f4227066b93e902365b3ad18e
Wilmer Lin <unitygameacademy@gmail.com>
2020-05-25 13:45:57 -0700

    added more ItemPickups to TriggerEvent scene

--- Assets/Scripts/Systems/PickupOnTriggerSystem.cs
+++ Assets/Scripts/Systems/PickupOnTriggerSystem.cs
@@ -8,6 +8,7 @@ using static Unity.Mathematics.math;
 using Unity.Physics;
 using Unity.Physics.Systems;
 
+[UpdateAfter(typeof(EndFramePhysicsSystem))] 
 public class PickupOnTriggerSystem : JobComponentSystem
 {
     private BuildPhysicsWorld buildPhysicsWorld;
@@ -20,71 +21,53 @@ public class PickupOnTriggerSystem : JobComponentSystem
         base.OnCreate();
         buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
         stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
-
         commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-
     }
 
+    [BurstCompile]
     struct PickupOnTriggerSystemJob : ITriggerEventsJob
     {
-        public EntityCommandBuffer entityCommandBuffer;
         [ReadOnly] public ComponentDataFromEntity<PickupTag> allPickups;
         [ReadOnly] public ComponentDataFromEntity<PlayerTag> allPlayers;
 
-        public bool isTriggered;
+        public EntityCommandBuffer entityCommandBuffer;
 
         public void Execute(TriggerEvent triggerEvent)
         {
             Entity entityA = triggerEvent.Entities.EntityA;
             Entity entityB = triggerEvent.Entities.EntityB;
 
-            // return if both Entities are triggers
             if (allPickups.Exists(entityA) && allPickups.Exists(entityB))
             {
                 return;
             }
 
-            if (isTriggered)
-            {
-                return;
-            }
-
-            // if Entity A is a pickup, and Entity B is the Player
             if (allPickups.Exists(entityA) && allPlayers.Exists(entityB))
             {
-
-                //UnityEngine.Debug.Log("Pickup entity A " + entityA + " has collided with Player entityB " + entityB);
+                //UnityEngine.Debug.Log("Pickup Entity A: " + entityA + " collided with Player Entity B: " + entityB);
                 entityCommandBuffer.DestroyEntity(entityA);
-
-
-                isTriggered = true;
-
             }
-            // if Entity A is the player, and Entity B is the PickUp
-            else if (allPickups.Exists(entityB) && allPlayers.Exists(entityA))
+            else if (allPlayers.Exists(entityA) && allPickups.Exists(entityB))
             {
-                //UnityEngine.Debug.Log("Player Entity A " + entityA + " has collided with Pickup entityB " + entityB);
+                //UnityEngine.Debug.Log("Player Entity A: " + entityA + " collided with PickUp Entity B: " + entityB);
                 entityCommandBuffer.DestroyEntity(entityB);
-                isTriggered = true;
             }
         }
     }
 
-
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
-        PickupOnTriggerSystemJob pickupJob = new PickupOnTriggerSystemJob();
-        pickupJob.allPlayers = GetComponentDataFromEntity<PlayerTag>(true);
-        pickupJob.allPickups = GetComponentDataFromEntity<PickupTag>(true);
-        pickupJob.entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+        var job = new PickupOnTriggerSystemJob();
+        job.allPickups = GetComponentDataFromEntity<PickupTag>(true);
+        job.allPlayers = GetComponentDataFromEntity<PlayerTag>(true);
+        job.entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
 
-        pickupJob.isTriggered = false;
-
-        JobHandle jobHandle = pickupJob.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld,
+        JobHandle jobHandle = job.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld,
             inputDependencies);
 
-        jobHandle.Complete();
 
+        //jobHandle.Complete();
+        commandBufferSystem.AddJobHandleForProducer(jobHandle);
         return jobHandle;
     }
 }
--- Assets/Scripts/Tags/PickupTag.cs
+++ Assets/Scripts/Tags/PickupTag.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -6,5 +6,19 @@ using Unity.Mathematics;
 [GenerateAuthoringComponent]
 public struct PickupTag : IComponentData
 {
-
+    // Add fields to your component here. Remember that:
+    //
+    // * A component itself is for storing data and doesn't 'do' anything.
+    //
+    // * To act on the data, you will need a System.
+    //
+    // * Data in a component must be blittable, which means a component can
+    //   only contain fields which are primitive types or other blittable
+    //   structs; they cannot contain references to classes.
+    //
+    // * You should focus on the data structure that mak es the most sense
+    //   for runtime use here. Authoring Components will be used for 
+    //   authoring the data in the Editor.
+    
+    
 }
